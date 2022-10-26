e6c28a691834fe15156ce93d3ce156c0501abca6
hey-ri <kwonhr01@naver.com>
2022-10-26 16:16:55 +0900

    addeventlistener로 뭔가를 만들긴 했는데,,

--- src/Image.js
+++ src/Image.js
@@ -1,15 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Image() {
     const [clickOn, setClickOn] = useState(false);
 
     const cake = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }];
 
-    const rotate = () => {
-        setClickOn(!clickOn);
-        const li = document.querySelector(".img_content");
-        !clickOn ? (li.style.transform = "rotateY(180deg)") : (li.style.transform = "rotateY(0deg)");
-    };
+    useEffect(() => {
+        const rotate = () => {
+            setClickOn(!clickOn);
+            const li = document.querySelector("li.img_content");
+            !clickOn ? (li.style.transform = "rotateY(180deg)") : (li.style.transform = "rotateY(0deg)");
+        };
+        document.addEventListener("click", rotate);
+        return () => {
+            document.removeEventListener("click", rotate);
+        };
+    }, [clickOn]);
 
     const scale = (e) => {
         e.target.style.transform = "scale(0.9)";
@@ -28,7 +34,7 @@ function Image() {
     return (
         <ul className="img_box">
             {cake.map((num) => (
-                <li className="img_content" onClick={rotate} onMouseEnter={scale} onMouseLeave={reset}>
+                <li className="img_content" onMouseEnter={scale} onMouseLeave={reset}>
                     <img key={num.id} src={"/img/" + num.id + ".jpg"} alt={num.id}></img>
                 </li>
             ))}
