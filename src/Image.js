5a7d04719bc049ec14316ef558f3d5c151a0c6a4
hey-ri <kwonhr01@naver.com>
2022-10-26 18:30:49 +0900

    바뀐 사항 없음

--- src/Image.js
+++ src/Image.js
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Image() {
     const [clickOn, setClickOn] = useState(false);
+    //const [allChange, setAllChange] = useState(false);
 
     const cake = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }];
 
-    useEffect(() => {
-        const rotate = () => {
-            setClickOn(!clickOn);
-            const li = document.querySelector("li.img_content");
-            !clickOn ? (li.style.transform = "rotateY(180deg)") : (li.style.transform = "rotateY(0deg)");
-        };
-        document.addEventListener("click", rotate);
-        return () => {
-            document.removeEventListener("click", rotate);
-        };
-    }, [clickOn]);
+    const ulR = useRef();
+
+    const rotate = () => {
+        setClickOn(!clickOn);
+        !clickOn ? (ulR.current.style.transform = "rotateY(180deg)") : (ulR.current.style.transform = "rotateY(0deg)");
+        console.log(ulR);
+    };
 
     const scale = (e) => {
         e.target.style.transform = "scale(0.9)";
@@ -25,17 +22,19 @@ function Image() {
         e.target.style.transform = "scale(1)";
     };
 
-    //이미지 클릭한 것으로 바뀌게 하기위한 로직 ing
+    const allimg = useRef();
+    //이미지 클릭한 것으로 바뀌게 하기위한 로직 미완성
     useEffect(() => {
         if (!clickOn) {
+            allimg.current = cake.id;
         }
     }, [clickOn]);
 
     return (
         <ul className="img_box">
             {cake.map((num) => (
-                <li className="img_content" onMouseEnter={scale} onMouseLeave={reset}>
-                    <img key={num.id} src={"/img/" + num.id + ".jpg"} alt={num.id}></img>
+                <li className="img_content" onClick={rotate} onMouseEnter={scale} onMouseLeave={reset} ref={ulR}>
+                    <img key={num.id} src={"/img/" + num.id + ".jpg"} alt={num.id} ref={allimg}></img>
                 </li>
             ))}
         </ul>
