85d3adc930f666986e00fdee17cda2fb0dfe2277
hey-ri <kwonhr01@naver.com>
2022-10-26 15:48:45 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Image from "./Image";
+
+function App() {
+    return (
+        <div>
+            <div className="container">
+                <h1>Click the photo</h1>
+                <Image></Image>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Image.js
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
+function Image() {
+    const [clickOn, setClickOn] = useState(false);
+
+    const cake = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }];
+
+    const rotate = () => {
+        setClickOn(!clickOn);
+        const li = document.querySelector(".img_content");
+        !clickOn ? (li.style.transform = "rotateY(180deg)") : (li.style.transform = "rotateY(0deg)");
+    };
+
+    const scale = (e) => {
+        e.target.style.transform = "scale(0.9)";
+    };
+
+    const reset = (e) => {
+        e.target.style.transform = "scale(1)";
+    };
+
+    //이미지 클릭한 것으로 바뀌게 하기위한 로직 ing
+    useEffect(() => {
+        if (!clickOn) {
+        }
+    }, [clickOn]);
+
+    return (
+        <ul className="img_box">
+            {cake.map((num) => (
+                <li className="img_content" onClick={rotate} onMouseEnter={scale} onMouseLeave={reset}>
+                    <img key={num.id} src={"/img/" + num.id + ".jpg"} alt={num.id}></img>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default Image;
