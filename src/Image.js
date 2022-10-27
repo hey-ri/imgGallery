a8f2681e9b3e06af213a38e73212125005564722
hey-ri <kwonhr01@naver.com>
2022-10-27 17:47:37 +0900

    전체 다 돌아가고 hover시 자동으로 돌아가는 문제 해결한 로직

--- src/Image.js
+++ src/Image.js
@@ -5,42 +5,52 @@ function Image() {
 
     const cake = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }];
 
-    const rotate = () => {
-        setClickOn((current) => !current);
+    const nomal = "0deg";
+    const reserve = "180deg";
+
+    const sScale = 0.9;
+    const nScale = 1;
+
+    const rotate = (e) => {
+        const liList = Array.from(e.target.closest("ul").children);
+        setClickOn((current) => {
+            liList.forEach((li) => {
+                li.style.setProperty("--r", current ? nomal : reserve);
+            });
+            return !current;
+        });
     };
 
-    const style = clickOn ? { transform: " rotateY(180deg" } : null;
-
     const scale = (e) => {
-        e.target.style.transform = "scale(0.9)";
+        e.target.style.setProperty("--s", sScale);
     };
 
     const reset = (e) => {
-        e.target.style.transform = "scale(1)";
+        e.target.style.setProperty("--s", nScale);
     };
 
     //이미지 클릭한 것으로 바뀌게 하기위한 로직 미완성
 
-    const allimg = useRef();
+    /*  const allimg = useRef();
 
     useEffect(() => {
-        if (style) {
+        if (current) {
             allimg.current = cake.id;
         }
-    }, [clickOn]);
+    }, [clickOn]); */
 
     return (
         <ul className="img_box">
-            {cake.map((num) => (
+            {cake.map(({ id }) => (
                 <li
                     className="img_content"
-                    key={num.id}
+                    key={id}
                     onClick={rotate}
                     onMouseEnter={scale}
                     onMouseLeave={reset}
-                    style={style}
+                    style={{ transform: `scale(var(--s)) rotateY(var(--r))` }}
                 >
-                    <img src={"/img/" + num.id + ".jpg"} alt={num.id} ref={allimg}></img>
+                    <img src={`/img/${id}.jpg`} alt={id}></img>
                 </li>
             ))}
         </ul>
