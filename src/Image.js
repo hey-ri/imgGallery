fae94986a179565880683834a6ec1b8d3cfdf33a
hey-ri <kwonhr01@naver.com>
2022-10-27 11:53:32 +0900

    전체 이미지가 돌아가는 로직 완성,, ㅠ

--- src/Image.js
+++ src/Image.js
@@ -2,17 +2,14 @@ import { useEffect, useRef, useState } from "react";
 
 function Image() {
     const [clickOn, setClickOn] = useState(false);
-    //const [allChange, setAllChange] = useState(false);
 
     const cake = [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }, { id: 8 }];
 
-    const ulR = useRef();
-
     const rotate = () => {
-        setClickOn(!clickOn);
-        !clickOn ? (ulR.current.style.transform = "rotateY(180deg)") : (ulR.current.style.transform = "rotateY(0deg)");
-        console.log(ulR);
+        setClickOn((current) => !current);
     };
+
+    const style = clickOn ? { transform: " rotateY(180deg" } : null;
 
     const scale = (e) => {
         e.target.style.transform = "scale(0.9)";
@@ -22,10 +19,12 @@ function Image() {
         e.target.style.transform = "scale(1)";
     };
 
-    const allimg = useRef();
     //이미지 클릭한 것으로 바뀌게 하기위한 로직 미완성
+
+    const allimg = useRef();
+
     useEffect(() => {
-        if (!clickOn) {
+        if (style) {
             allimg.current = cake.id;
         }
     }, [clickOn]);
@@ -33,8 +32,15 @@ function Image() {
     return (
         <ul className="img_box">
             {cake.map((num) => (
-                <li className="img_content" onClick={rotate} onMouseEnter={scale} onMouseLeave={reset} ref={ulR}>
-                    <img key={num.id} src={"/img/" + num.id + ".jpg"} alt={num.id} ref={allimg}></img>
+                <li
+                    className="img_content"
+                    key={num.id}
+                    onClick={rotate}
+                    onMouseEnter={scale}
+                    onMouseLeave={reset}
+                    style={style}
+                >
+                    <img src={"/img/" + num.id + ".jpg"} alt={num.id} ref={allimg}></img>
                 </li>
             ))}
         </ul>
