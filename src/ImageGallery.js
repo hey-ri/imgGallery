de965583493546e1853d9b5ca0c860c7b98379f4
hey-ri <kwonhr01@naver.com>
2022-11-02 09:58:11 +0900

    useImageGallery만들어서 빼기 전 상태

--- src/ImageGallery.js
+++ src/ImageGallery.js
@@ -1,5 +1,19 @@
 import GalleryItem from "./GalleryItem";
-import useImageGallery from "./hooks/useImageGallery";
+import { useCallback, useState } from "react";
+function useImageGallery(dep) {
+    // 각각의 이미지의 앞면을 보여주는 상태
+    const [isFront, setIsFront] = useState(false);
+
+    const [selectedItemUrl, setSelectedItemUrl] = useState("");
+
+    const onContentSelect = useCallback((url) => {
+        // 상태값 토글
+        setIsFront((v) => !v);
+        setSelectedItemUrl(url);
+    }, dep);
+
+    return { isFront, selectedItemUrl, onContentSelect };
+}
 
 const nomal = "0deg";
 const reverse = "180deg";
